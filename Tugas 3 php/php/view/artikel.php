6522d3c9bfad33b05b475b6e973cbca46d8b231e
ganeshariannihans/web-programming-2022 <92968712+ganeshariannihans@users.noreply.github.com>
2022-03-16 13:14:18 -0400

    Add files via upload

---
+++ Tugas 3 php/php/view/artikel.php
@@ -0,0 +1,118 @@
+<?php
+include '../layout/header.php';
+?>
+
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="jumbotron">
+                <h2>
+                    About Birds
+                </h2>
+                <p>
+                Many can also run, jump, swim, and dive. Some, like penguins, have lost the ability to fly but retained their wings.
+                </p>
+                <p>
+                    <a class="btn btn-primary btn-large" href="#">Learn more</a>
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+            <img src="https://www.all-birds.com/picPg/bluebird-M-827-pL.JPG" alt="">
+                    <div class="card-block">
+                        <h5 class="card-title">
+                            Bird
+
+                        </h5>
+                        <p class="card-text">
+                            Birds are found worldwide and in all habitats.
+
+                            The largest is the nine-foot-tall ostrich. The smallest is the two-inch-long bee hummingbird.
+
+                            Everything about the anatomy of a bird reflects its ability to fly.
+                        </p>
+                        <p>
+                            <a class="btn btn-primary" href="#">Action</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+                    <div class="card">
+            <img src="https://news.stanford.edu/wp-content/uploads/2020/10/Birds_Culture-1-copy.jpg" alt="">
+                    <div class="card-block">
+                        <h5 class="card-title">
+                             Birds Culture
+
+                        </h5>
+                        <p class="card-text">
+                        The wings, for example, are shaped to create lift.
+
+                        The leading edge is thicker than the back edge, and they are covered in feathers that narrow to a point.
+
+                        Airplane wings are modeled after bird wings.
+                        </p>
+                        <p>
+                            <a class="btn btn-primary" href="#">Action</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+                    <div class="col-md-4">
+                        <div class="card">
+            <img src="https://cdn.birdwatchingdaily.com/2020/06/Mountain-Bluebird-nr-Millarville-29-May-2020.jpg" alt="">
+                    <div class="card-block">
+                        <h5 class="card-title">
+                            blue birds
+
+                        </h5>
+                        <p class="card-text">
+                        The bones and muscles of the wing are also highly specialized. 
+
+                        The main bone, the humerus, which is similar to the upper arm of a mammal, is hollow instead of solid.
+
+                        <p>
+                            <a class="btn btn-primary" href="#">Action</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+                    <div class="col-md-6">
+                        <dl>
+                            <dt>
+                                Birds are vertebrate animals adapted for flight.
+                            </dit>
+                            <dd>
+
+                                It also connects to the bird’s air sac system, which, in turn, connects to its lungs.
+
+                                The powerful flight muscles of the shoulder attach to the keel, a special ridge of bone that runs down the center of the wide sternum, or breastbone. 
+
+                                The tail feathers are used for steering.
+                            </dd>
+                            <dt>
+                                They use their beaks to grab and swallow food.
+                            </dl>
+                            <dd>
+                                Birds have a unique digestive system that allows them to eat when they can—usually on the fly—and digest later.
+
+                                Even the way a bird reproduces is related to flight.
+                            </dd>
+                        </dl>
+                            <p class="text-center text-succes">
+                                Instead of carrying the extra weight of developing young inside their bodies, they lay eggs and incubate them in a nest.
+
+                             The fossil record shows that birds evolved alongside the dinosaurs during the Jurassic period 160 million years ago.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <?php
+
+                include'../layout/footer.php';
+                ?>
+                    
