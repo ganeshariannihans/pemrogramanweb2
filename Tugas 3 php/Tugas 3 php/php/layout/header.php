a8112a695255c4d68f1ede5819a14f1525d2f068
ganeshariannihans/web-programming-2022 <92968712+ganeshariannihans@users.noreply.github.com>
2022-03-17 03:09:26 -0400

    Add files via upload

---
+++ Tugas 3 php/Tugas 3 php/php/layout/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Tugas3</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet"/>
+    <link href="css/styles.css" rel="styleshet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFIdvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jlW3" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+</head>
+<body class="sb-nav-fixed">
+          <nav class="sb-topnav navbar navbar-expand navbar-warning bg-warning">
+              <!-- Navbar Brand-->
+              <a class="navbar-brand ps-3" href="index.html">Star Bootstrap</a>
+              <!-- Sidebar Toggle-->
+              <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebartoggle" href="#!"></button>
+              <!-- Navbar Search-->
+              <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-md-0">
+                  <div class="input-group">
+                      <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                      <button class="btn btn-danger" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                  </div>
+              </form>
+
+              <!-- Navbar-->
+              <ul class="navbar-nav ms-md-0 me-3 me-lg-4">
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class=fas fa-user fa-fw"></i></a>
+                      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="#!">Settings</a></li>
+                          <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                          <li><hr class="dropdown-divider"/></li>
+                          <li><a class="dropdown-item" href="#!">Logout</a></li>
+                      </ul>
+                  </li>
+               </ul>
+            </nav>
+
+            
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+<script src="assets/demo/chart-area-demo.js"></script>
+<script src="assets/demo/chart-bar-demo.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+<script src="js/datatables-simple-demo.js"></script>
+</body>
+</html>
